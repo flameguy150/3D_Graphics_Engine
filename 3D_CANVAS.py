71cfcbd19266205066b86e2d8fd588b8e788e3f8
Justin Lee <flameguy150@gmail.com>
2024-10-07 00:23:12 -0500

    grid lines follow axis lines and working on increments based on window size

--- 3D_CANVAS.py
+++ 3D_CANVAS.py
@@ -14,7 +14,11 @@ root.title('3D Engine')
 #need to resize canvas so it fits with window size
 #get new coordinates of window by winfo_screenwidth() and winfo_screenheight()
 
-canvas = tk.Canvas(root, width=600, height=400, bg='black')
+root.minsize(600, 400)
+root.maxsize(1536, 864)
+
+
+canvas = tk.Canvas(root, width=600, height=400, bg='navy')
 
 radius = 50
 middle_w = 1/2 * int(canvas['width'])
@@ -27,10 +31,26 @@ main_y2 = root.winfo_height()
 
 can_width = canvas.winfo_width()
 can_height = canvas.winfo_height()
+
 crosshair = canvas.create_oval(middle_w - radius, middle_h - radius, middle_w + radius, middle_h + radius, fill="red", outline="blue", width="1")
 
 axis_x = canvas.create_line(main_x1, middle_h, canvas['width'], middle_h, fill="white")
 axis_y = canvas.create_line(middle_w, main_y1, middle_w, canvas['height'], fill="white")
+
+# print(root.winfo_screenwidth)
+# print(root.winfo_screenheight)
+
+# originxx = canvas.create_text(1/2 * int(canvas['width']) + 5, 1/2 * int(canvas['height']) + 5, text="O")
+
+#gridpoints
+ticks = []
+tecks = []
+for x in range (int(-middle_w), int(middle_w) + 1, 50):
+    ticks.append(canvas.create_line(middle_w + x, int(canvas['height'])/2, middle_w + x + 2, int(canvas['height'])/2, width=10, fill="white")) 
+    tecks.append(canvas.create_text(middle_w + x, int(canvas['height'])/2 + 10, text=str(x)))
+for y in range (int(-middle_h), int(middle_h) + 1, 50):
+    ticks.append(canvas.create_line(int(canvas['width'])/2, middle_h + y, int(canvas['width'])/2, middle_h + y + 2, width=10, fill="white")) 
+    tecks.append(canvas.create_text(int(canvas['width'])/2 + 10, middle_h + y, text=str(y), fill="white"))
 
 
 def can_resize(event):
@@ -41,6 +61,8 @@ def can_resize(event):
     global crosshair
     global axis_x
     global axis_y
+    global ticks
+    global tecks
     print(root.winfo_width(), root.winfo_height())
     canvas.config(width=root.winfo_width(), height=root.winfo_height())
     canvas.delete(crosshair)
@@ -51,14 +73,106 @@ def can_resize(event):
                                     fill="red", outline="blue", width="1")
     
     canvas.delete(axis_x, axis_y)
-    axis_x = canvas.create_line(main_x1, int(canvas['height'])/2 , canvas['width'], int(canvas['height'])/2, fill="white")
-    axis_y = canvas.create_line(int(canvas['width'])/2 , main_y1, int(canvas['width'])/2 , canvas['height'], fill="white")
+    axis_x = canvas.create_line(main_x1, int(canvas['height'])/2, 
+                                canvas['width'], 
+                                int(canvas['height'])/2,
+                                fill="black")
+    axis_y = canvas.create_line(int(canvas['width'])/2,
+                                main_y1,
+                                int(canvas['width'])/2,
+                                canvas['height'],
+                                fill="black")
+    # canvas.delete(originxx)
+    # originxx = canvas.create_text(int(canvas['width']/2) + 5, int(canvas['height']/2) + 5, text="O")
 
+    #delete all gridpoints and text
+    for tick in ticks:
+        canvas.delete(tick)
+    for teck in tecks:       
+        canvas.delete(teck)
+
+    ticks = []
+    tecks = []
+
+    #for tick in ticks:
+        #canvas.delete(...)
+
+    # for x in range (int(-middle_w), int(middle_w) + 1, 50):
+    #     #ticks.append(canvas.create_....)
+    #     gridpoint_x = canvas.create_line(middle_w + x, int(canvas['height'])/2, middle_w + x + 2, int(canvas['height'])/2, width=10, fill="white")  
+    #     gridtext_x = canvas.create_text(middle_w + x, int(canvas['height'])/2 + 10, text=str(x), fill="white")
+    # for y in range (int(-middle_h), int(middle_h) + 1, 50):
+    #     gridpoint_y = canvas.create_line(int(canvas['width'])/2, middle_h + y, int(canvas['width'])/2, middle_h + y + 2, width=10, fill="white") 
+    #     gridtext_y = canvas.create_text(int(canvas['width'])/2 + 10, middle_h + y, text=str(y), fill="white")
+
+    #set max and min grid increment
+    #max = 5
+    #min = 50
+
+    #600X400
+    #100% = 50
+    #min = 5
+
+    #600 = 50
+    # 50
+    # 45
+    # 40
+    # 35
+    # 30
+    # 25
+    # 20
+    # 15
+    # 10
+    # 5
+    #1536 = 5
+
+    #get percentage/ratio to determine increments based on window size
+    #round number to nice number divisble by 5
+    
+    #x-axis min=600 max=winfo.screen_width()
+    # increments = x + 5
+    
+    # x = canvas['width']/
+
+    # max = 1536
+    # min = 600
+
+    # diff = 936
+    # percentjohn = (int(canvas['width'])-600) / 936
+    # percentjohn * 45
+
+
+    # DEBUG HERE
+    # DEBUG HERE
+
+    percentjohn = (int(canvas['width'])-600) / 936
+    percentjlee = percentjohn * 45
+    increments = int(percentjlee + 5)
+    
+    # DEBUG HERE
+    # DEBUG HERE
+    
+    
+
+
+    for x in range (int(-middle_w), int(middle_w) + 1, increments):
+        ticks.append(canvas.create_line(middle_w + x, int(canvas['height'])/2, middle_w + x + 2, int(canvas['height'])/2, width=10, fill="white")) 
+        tecks.append(canvas.create_text(middle_w + x, int(canvas['height'])/2 + 10, text=str(x), fill="white"))
+    for y in range (int(-middle_h), int(middle_h) + 1, increments):
+        ticks.append(canvas.create_line(int(canvas['width'])/2, middle_h + y, int(canvas['width'])/2, middle_h + y + 2, width=10, fill="white")) 
+        tecks.append(canvas.create_text(int(canvas['width'])/2 + 10, middle_h + y, text=str(y), fill="white"))
 root.bind("<Configure>", can_resize)
 
 
-#create_line multiple times
+#create_line multiple times with for loop
+#just need to change x1 and x2 to shift
+#good width for lines is 10, distance for x1 and x2 should be about 2
 
+# gridpoint = canvas.create_line(0, int(canvas['height'])/2, 2, int(canvas['height'])/2, width=10, fill="green")
+# gridpoint = canvas.create_line(10, int(canvas['height'])/2, 12, int(canvas['height'])/2, width=10, fill="green")
+# gridpoint = canvas.create_line(20, int(canvas['height'])/2, 22, int(canvas['height'])/2, width=10, fill="green")
+
+  
 
 def mouse_print(event):
     print(event.x, event.y)
@@ -87,10 +201,10 @@ canvas.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
 
 
 #seems right
-print(middle_h)
-print(middle_w)
-print(can_width)
-print(can_height)
+# print(middle_h)
+# print(middle_w)
+# print(can_width)
+# print(can_height)
 
 #parameters of create_ovals (x1,y1) = top left point of box of circle, (x2, y2) = bottom right point of box of circle
 # crosshair.place(relx = 0, rely = 0, relwidth = 1, relheight = 1) does not work
